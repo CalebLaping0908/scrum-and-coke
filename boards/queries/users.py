797c4d1f1537f292f45dc578a75dc40388499b89
elizabethleah22 <elizabeth.leahgonz@gmail.com>
2023-02-20 12:07:56 -0700

    Connected to users database and added a response model to the post request, as well as an error code if the post data is invalid

--- boards/queries/users.py
+++ boards/queries/users.py
@@ -1,7 +1,42 @@
 from pydantic import BaseModel
+from queries.pool import pool
+
+class Error(BaseModel):
+    message: str
 
 class UserIn(BaseModel):
     email: str
     full_name: str
     password: str
     employee_number: int
+
+class UserOut(BaseModel):
+    id: int
+    email: str
+    full_name: str
+    password: str
+    employee_number: int
+
+class UserRepository:
+    def create(user: UserIn) -> UserOut:
+        with pool.connection() as conn:
+            with conn.cursor() as db:
+                result = db.execute(
+                    """
+                    INSERT INTO users
+                    (email, full_name, password, employee_number)
+                    VALUES
+                    (%s, %s, %s, %s)
+                    RETURNING id;
+                    """,
+                    [
+                        user.email,
+                        user.full_name,
+                        user.password,
+                        user.employee_number
+                    ]
+                )
+                id = result.fetchone()[0]
+                old_data = user.dict()
+                return {"message": "invalid input"}
+                return UserOut(id=id, **old_data)
--- boards/routers/users.py
+++ boards/routers/users.py
@@ -1,8 +1,14 @@
-from fastapi import APIRouter
-from queries.users import UserIn
+from fastapi import APIRouter, Depends, Response
+from typing import Union
+from queries.users import UserIn, UserRepository, UserOut, Error
 
 router = APIRouter()
 
-@router.post("/users")
-def create_user(user: UserIn):
-    return user
+@router.post("/users", response_model=Union[UserOut, Error])
+def create_user(
+    user: UserIn,
+    response: Response,
+    repo: UserRepository = Depends()
+):
+    response.status_code = 400
+    return repo.create(user)
