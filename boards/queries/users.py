5f4e2159a479b896ac4b98b1f266a346bb42d17d
elizabethleah22 <elizabeth.leahgonz@gmail.com>
2023-02-20 14:41:34 -0700

    Get all users request successful

--- boards/queries/users.py
+++ boards/queries/users.py
@@ -1,5 +1,9 @@
 from pydantic import BaseModel
 from queries.pool import pool
+from typing import List, Union
+
+class Error(BaseModel):
+    message: str
 
 
 class UserIn(BaseModel):
@@ -16,7 +20,34 @@ class UserOut(BaseModel):
     employee_number: int
 
 class UserRepository:
-    def create(self, user: UserIn) -> UserOut:
+    def get_all(self) -> Union[Error, List[UserOut]]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    db.execute(
+                        """
+                        SELECT id, email, full_name, password, employee_number
+                        FROM users
+                        ORDER BY id
+
+                        """
+                    )
+                    return [
+                        UserOut(
+                        id=record[0],
+                        email=record[1],
+                        full_name=record[2],
+                        password=record[3],
+                        employee_number=record[4]
+                    )
+                    for record in db
+                    ]
+
+        except Exception:
+            return {"message": "could not get all users"}
+
+
+    def create(self, user: UserIn) -> Union[Error,UserOut]:
         with pool.connection() as conn:
             with conn.cursor() as db:
                 result = db.execute(
--- boards/routers/users.py
+++ boards/routers/users.py
@@ -1,11 +1,19 @@
 from fastapi import APIRouter, Depends
-from queries.users import UserIn, UserRepository, UserOut
+from typing import List, Union
+from queries.users import UserIn, UserRepository, UserOut, Error
 
 router = APIRouter()
 
-@router.post("/users", response_model=UserOut)
+@router.post("/users", response_model=Union[UserOut, Error])
 def create_user(
     user: UserIn,
     repo: UserRepository = Depends()
 ):
     return repo.create(user)
+
+
+@router.get("/users", response_model=Union[Error, List[UserOut]])
+def get_all(
+    repo: UserRepository = Depends(),
+    ):
+    return repo.get_all()
