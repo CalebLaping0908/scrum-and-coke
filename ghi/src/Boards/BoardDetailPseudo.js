b4d2cdefc3ea01ff56b0592e7829476fe6846759
elizabethleah22 <elizabeth.leahgonz@gmail.com>
2023-03-01 13:42:00 -0700

    Progress on patch request

--- ghi/src/Boards/BoardDetailPseudo.js
+++ ghi/src/Boards/BoardDetailPseudo.js
@@ -1,76 +1,87 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-export default function BoardDetail({ tasks, getTasks, boards, getBoards, statuses }){
-  const [taskStatus, setTaskStatus] = useState('');
-  const [boardNumVar, setBoardNumVar] = useState('');
+export default function BoardDetail({ tasks, getTasks, boards, getBoards }) {
+  const [taskStatus, setTaskStatus] = useState("");
+  const [boardNumVar, setBoardNumVar] = useState("");
 
-  
-  const updateTask = async (id) => {
+  const updateTask = async (id, status) => {
     const data = {};
-    console.log("ID", id);
     data.status = taskStatus;
-    console.log("DATA", data);
 
     const taskUrl = `http://localhost:8080/tasks/${id}/`;
     const fetchConfig = {
-        method: "PATCH",
-        body: JSON.stringify(data),
-        headers: {
-        'Content-Type': 'application/json',
-        },
+      method: "PATCH",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
     };
 
-  const response = await fetch(taskUrl, fetchConfig);
-  if (response.ok) {
-    const task = await response.json();
-    console.log(task);
-    setTaskStatus('');
-    // getTasks();
-  }
-  }
+    const response = await fetch(taskUrl, fetchConfig);
+    if (response.ok) {
+      const task = await response.json();
+      console.log(task);
+      setTaskStatus("");
+      // getTasks();
+    }
+  };
   if (tasks === undefined) {
-     return null
+    return null;
   }
-
 
   const handleTaskStatus = async (event) => {
-    const value = event.target.value;
-    setTaskStatus(value);
-    updateTask();
-    }
-  
+    let statusString = event.target.value;
+    let statusArray = statusString.split(",");
+    let status = statusArray[0];
+    const id = statusArray[1];
+    console.log("status", status);
+    console.log("id", id);
+    setTaskStatus(status);
+    updateTask(id, status);
+  };
+
   const handleBoardNumVarChange = async (event) => {
     const value = event.target.value;
     setBoardNumVar(value);
-    }
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    setBoardNumVar('');
-    getBoards();        
-    
-  }
-
+    setBoardNumVar("");
+    getBoards();
+    updateTask();
+  };
 
   return (
     <>
-          <div><br></br></div>
+      <div>
+        <br></br>
+      </div>
       <form onSubmit={handleSubmit} id="select-board-form">
-              <div className="mb-3">
-                <div className="form-floating mb-3">
-                <select onChange={handleBoardNumVarChange} placeholder="Board" required type="text"  name="boardNumVar" id="boardNumVar" className="form-select" value={boardNumVar}>
-                  <option>Board</option>
-                  {boards.map(board => {
-                    return (
-                        <option key={board.id} value={board.id}>
-                            {board.name}
-                        </option>
-                    );
-                  })}
-                </select>
-                </div>
-              </div>
-      </form> 
+        <div className="mb-3">
+          <div className="form-floating mb-3">
+            <select
+              onChange={handleBoardNumVarChange}
+              placeholder="Board"
+              required
+              type="text"
+              name="boardNumVar"
+              id="boardNumVar"
+              className="form-select"
+              value={boardNumVar}
+            >
+              <option>Board</option>
+              {boards.map((board) => {
+                return (
+                  <option key={board.id} value={board.id}>
+                    {board.name}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+        </div>
+      </form>
 
       {/* Once the state of boardNumVar is selected, use it to filter tasks for the board display */}
       <table className="table table-striped align-middle mt-5">
@@ -78,14 +89,14 @@ export default function BoardDetail({ tasks, getTasks, boards, getBoards, status
           <tr>
             <th>Backlog</th>
             <th>To Do</th>
-            <th>In Progress</th> 
-            <th>In Review/QA</th> 
-            <th>Complete</th>             
+            <th>In Progress</th>
+            <th>In Review/QA</th>
+            <th>Complete</th>
           </tr>
         </thead>
         <tbody>
-            {/* <td>Backlog */}
-                {/* <div class="card">
+          {/* <td>Backlog */}
+          {/* <div class="card">
                 <div class="card-body">
                     {tasks.filter(task => task.status == "Backlog" && task.board.id == boardNumVar).map(task => {
                             return (
@@ -99,12 +110,9 @@ export default function BoardDetail({ tasks, getTasks, boards, getBoards, status
                 </div>
                     )} */}
 
-
-
-
-                {/* <th>Title</th>
+          {/* <th>Title</th>
                 <th>Description</th>
-                <th>Assignee</th> 
+                <th>Assignee</th>
                 <th>Status</th>
                     <tbody>
                         {tasks.filter(task => task.status == "Backlog" && task.board.id == boardNumVar).map(task => {
@@ -124,16 +132,16 @@ export default function BoardDetail({ tasks, getTasks, boards, getBoards, status
                                     );
                                     })}
                                     </select>
-                                </td>   
+                                </td>
                             </tr>
                             );
                         })}
                     </tbody>
-            </td> 
+            </td>
             <td>To Do
                 <th>Title</th>
                 <th>Description</th>
-                <th>Assignee</th> 
+                <th>Assignee</th>
                 <th>Status</th>
                     <tbody>
                         {tasks.filter(task => task.status == "To Do" && task.board.id == boardNumVar).map(task => {
@@ -153,47 +161,69 @@ export default function BoardDetail({ tasks, getTasks, boards, getBoards, status
                                     );
                                     })}
                                     </select>
-                                </td>  
+                                </td>
                             </tr>
                             );
                         })}
                     </tbody>
             </td> */}
-            <td>
+          <td>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Assignee</th>
+            <th>Status</th>
+            <tbody>
+              {tasks
+                .filter(
+                  (task) =>
+                    task.status == "In Progress" && task.board == boardNumVar
+                )
+                .map((task) => {
+                  return (
+                    <tr key={task.id}>
+                      <td>{task.title}</td>
+                      <td>{task.description}</td>
+                      <td>{task.assignee}</td>
+                      <td>
+                        <form
+                          onSubmit={updateTask(task.id)}
+                          id="task-status-form"
+                        >
+                          <select
+                            onChange={handleTaskStatus}
+                            defaultValue={[task.status, task.id]}
+                            required
+                            type="text"
+                            name="taskStatus"
+                            id="taskStatus"
+                            className="form-select"
+                            value={[task.status, task.id]}
+                          >
+                            <option value={["Backlog", task.id]}>
+                              Backlog
+                            </option>
+                            <option value={["To Do", task.id]}>To Do</option>
+                            <option value={[task.status, task.id]}>
+                              In Progress
+                            </option>
+                            <option value={["In Review / QA", task.id]}>
+                              In Review / QA
+                            </option>
+                            <option value={["Completed", task.id]}>
+                              Completed
+                            </option>
+                          </select>
+                        </form>
+                      </td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </td>
+          {/* <td>In Review/QA
                 <th>Title</th>
                 <th>Description</th>
-                <th>Assignee</th> 
-                <th>Status</th>
-                    <tbody>
-                        {tasks.filter(task => task.status == "In Progress" && task.board == boardNumVar).map(task => {
-                            return (
-                            <tr key={task.id}>
-                                <td>{ task.title }</td>
-                                <td>{ task.description }</td>
-                                <td>{ task.assignee }</td>                              
-                                <td>
-                                      {/* <form onSubmit={updateTask(task.id)} id="task-status-form">   */}
-                                        <select onChange={handleTaskStatus} placeholder= {task.status} required type="text"  name="taskStatus" id="taskStatus" className="form-select" value={taskStatus}>
-                                        <option>Status</option>
-                                        {statuses.map(status => {
-                                        return (
-                                        <option key={status.id} value={task.status}>
-                                            {status.status}
-                                        </option>
-                                        );
-                                        })}
-                                        </select>
-                                      {/* </form>                                       */}
-                                </td>   
-                            </tr>
-                            );
-                        })}
-                    </tbody>
-            </td>
-            {/* <td>In Review/QA
-                <th>Title</th>
-                <th>Description</th>
-                <th>Assignee</th> 
+                <th>Assignee</th>
                 <th>Status</th>
                     <tbody>
                         {tasks.filter(task => task.status == "In Review / QA" && task.board.id == boardNumVar).map(task => {
@@ -213,7 +243,7 @@ export default function BoardDetail({ tasks, getTasks, boards, getBoards, status
                                     );
                                     })}
                                     </select>
-                                </td>  
+                                </td>
                             </tr>
                             );
                         })}
@@ -222,7 +252,7 @@ export default function BoardDetail({ tasks, getTasks, boards, getBoards, status
             <td>Complete!
                 <th>Title</th>
                 <th>Description</th>
-                <th>Assignee</th> 
+                <th>Assignee</th>
                 <th>Status</th>
                     <tbody>
                         {tasks.filter(task => task.status == "Completed" && task.board.id == boardNumVar).map(task => {
@@ -242,14 +272,14 @@ export default function BoardDetail({ tasks, getTasks, boards, getBoards, status
                                     );
                                     })}
                                     </select>
-                                </td>   
+                                </td>
                             </tr>
                             );
                         })}
                     </tbody>
             </td> */}
         </tbody>
-      </table>      
+      </table>
     </>
-    );
+  );
 }
