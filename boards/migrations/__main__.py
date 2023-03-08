92d36d53da894f9e634cde9e5875a68e9462948c
Caleb <cmlaping@gmail.com>
2023-03-08 15:56:20 -0700

    Created dockerfile line

--- boards/migrations/__main__.py
+++ boards/migrations/__main__.py
@@ -2,9 +2,9 @@ async def migrate():
     from . import down, up, LATEST, ZERO
     import os
     import sys
-    print('I HAVE GOTTEN INSIDE OF MIGRATIONS/MAIN.PY')
+
     db_url = os.environ["DATABASE_URL"]
-    print(db_url, 'THIS IS OUR DB URL INSIDE MIGRATIONS/MAIN.PY')
+
     if len(sys.argv) < 2:
         print("Command: up|down [amount]")
         exit(1)
